f74fbd1d2a292f7a31d6d9b1af546a4096a77eee
Eveline Cai <33134427+evelinec@users.noreply.github.com>
2018-12-11 15:37:08 -0500

    Added echo to commands

--- kubernetes-test-script.sh
+++ kubernetes-test-script.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
+echo "mvn -q package"
 mvn -q package
+
+echo "kubectl apply -f kubernetes.yaml"
 kubectl apply -f kubernetes.yaml
+
+echo "sleep 120"
 sleep 120
+
+echo "kubectl get pods"
 kubectl get pods
+
+echo "minikube ip"
 echo `minikube ip`
+
+echo "curl http://`minikube ip`:31000/api/name"
 curl http://`minikube ip`:31000/api/name
+
+echo "curl http://`minikube ip`:32000/api/ping/name-service"
 curl http://`minikube ip`:32000/api/ping/name-service
+
+echo "mvn verify -Ddockerfile.skip=true -Dcluster.ip=`minikube ip`"
 mvn verify -Ddockerfile.skip=true -Dcluster.ip=`minikube ip`
+
+echo "kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep name)"
 kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep name)
+
+echo "kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep ping)" 
 kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep ping)
