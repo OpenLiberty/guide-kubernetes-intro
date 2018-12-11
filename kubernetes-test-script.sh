0446962fe7d8ff177647d86f3c5d32af5a653143
Eveline Cai <33134427+evelinec@users.noreply.github.com>
2018-12-11 15:54:40 -0500

    Added printf for commands

--- kubernetes-test-script.sh
+++ kubernetes-test-script.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-echo "mvn -q package"
+printf "\nmvn -q package"
 mvn -q package
 
-echo "kubectl apply -f kubernetes.yaml"
+printf "\nkubectl apply -f kubernetes.yaml"
 kubectl apply -f kubernetes.yaml
 
-echo "sleep 120"
+printf "\nsleep 120"
 sleep 120
 
-echo "kubectl get pods"
+printf "\nkubectl get pods"
 kubectl get pods
 
-echo "minikube ip"
+printf "\nminikube ip"
 echo `minikube ip`
 
-echo "curl http://`minikube ip`:31000/api/name"
+printf "\ncurl http://`minikube ip`:31000/api/name"
 curl http://`minikube ip`:31000/api/name
 
-echo "curl http://`minikube ip`:32000/api/ping/name-service"
+printf "\ncurl http://`minikube ip`:32000/api/ping/name-service"
 curl http://`minikube ip`:32000/api/ping/name-service
 
-echo "mvn verify -Ddockerfile.skip=true -Dcluster.ip=`minikube ip`"
+printf "\nmvn verify -Ddockerfile.skip=true -Dcluster.ip=`minikube ip`"
 mvn verify -Ddockerfile.skip=true -Dcluster.ip=`minikube ip`
 
-echo "kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep name)"
+printf "\nkubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep name)"
 kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep name)
 
-echo "kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep ping)" 
+printf "\nkubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep ping)" 
 kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep ping)
