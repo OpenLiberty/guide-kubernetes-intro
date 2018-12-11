38c597ddbdcdaa038147c1d0d272a36b312b18d3
Eveline Cai <33134427+evelinec@users.noreply.github.com>
2018-12-11 16:02:45 -0500

    Update kubernetes-test-script.sh

--- kubernetes-test-script.sh
+++ kubernetes-test-script.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-printf "\nmvn -q package"
+printf "\nmvn -q package\n"
 mvn -q package
 
-printf "\nkubectl apply -f kubernetes.yaml"
+printf "\nkubectl apply -f kubernetes.yaml\n"
 kubectl apply -f kubernetes.yaml
 
-printf "\nsleep 120"
+printf "\nsleep 120\n"
 sleep 120
 
-printf "\nkubectl get pods"
+printf "\nkubectl get pods\n"
 kubectl get pods
 
-printf "\nminikube ip"
+printf "\nminikube ip\n"
 echo `minikube ip`
 
-printf "\ncurl http://`minikube ip`:31000/api/name"
+printf "\ncurl http://`minikube ip`:31000/api/name\n"
 curl http://`minikube ip`:31000/api/name
 
-printf "\ncurl http://`minikube ip`:32000/api/ping/name-service"
+printf "\ncurl http://`minikube ip`:32000/api/ping/name-service\n"
 curl http://`minikube ip`:32000/api/ping/name-service
 
-printf "\nmvn verify -Ddockerfile.skip=true -Dcluster.ip=`minikube ip`"
+printf "\nmvn verify -Ddockerfile.skip=true -Dcluster.ip=`minikube ip`\n"
 mvn verify -Ddockerfile.skip=true -Dcluster.ip=`minikube ip`
 
-printf "\nkubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep name)"
+printf "\nkubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep name)\n"
 kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep name)
 
-printf "\nkubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep ping)" 
+printf "\nkubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep ping)\n" 
 kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep ping)
