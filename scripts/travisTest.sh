66b6882e735b01186bfd1961fc15d38e213b045c
Panagiotis Roubatsis <panagiotis.roubatsis@ibm.com>
2019-04-11 10:19:17 -0400

    Fix Travis script

--- scripts/travisTest.sh
+++ scripts/travisTest.sh
@@ -21,17 +21,17 @@ kubectl get pods
 printf "\nminikube ip\n"
 echo `minikube ip`
 
-printf "\ncurl http://`minikube ip`:31000/api/name\n"
-curl http://`minikube ip`:31000/api/name
+printf "\ncurl http://`minikube ip`:31000/system/properties\n"
+curl http://`minikube ip`:31000/system/properties
 
-printf "\ncurl http://`minikube ip`:32000/api/ping/name-service\n"
-curl http://`minikube ip`:32000/api/ping/name-service
+printf "\ncurl http://`minikube ip`:32000/inventory/systems\n"
+curl http://`minikube ip`:32000/inventory/systems
 
 printf "\nmvn verify -Ddockerfile.skip=true -Dcluster.ip=`minikube ip`\n"
 mvn verify -Ddockerfile.skip=true -Dcluster.ip=`minikube ip`
 
-printf "\nkubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep name)\n"
-kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep name)
+printf "\nkubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep system)\n"
+kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep system)
 
-printf "\nkubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep ping)\n" 
-kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep ping)
+printf "\nkubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep inventory)\n" 
+kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep inventory)
