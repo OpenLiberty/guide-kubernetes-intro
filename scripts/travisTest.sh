6f21f063938cc8839bc4e18b0fd0ff8bc6c6b03e
evelinec <evelinec@ca.ibm.com>
2020-01-14 14:26:24 -0500

    Fixed command substitutions

--- scripts/travisTest.sh
+++ scripts/travisTest.sh
@@ -20,12 +20,12 @@ sleep 120
 
 kubectl get pods
 
-echo `minikube ip`
+echo $(minikube ip)
 
 curl http://`minikube ip`:31000/system/properties
 curl http://`minikube ip`:32000/inventory/systems
 
-mvn failsafe:integration-test -Ddockerfile.skip=true -Dcluster.ip=`minikube ip`
+mvn failsafe:integration-test -Ddockerfile.skip=true -Dcluster.ip=$(minikube ip)
 mvn failsafe:verify
 
 kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep system)
