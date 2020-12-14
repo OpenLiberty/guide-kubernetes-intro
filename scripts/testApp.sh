f790155dc2839bb728ce4d1697b974d15b5ce3c3
Tien Thanh Le <44335153+tt-le@users.noreply.github.com>
2020-12-14 13:22:57 -0500

    Update testApp.sh

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 # Set up and start Minikube
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x kubectl
-sudo ln -s $(pwd)/kubectl /usr/local/bin/kubectl
+sudo ln -s -f $(pwd)/kubectl /usr/local/bin/kubectl
 wget https://github.com/kubernetes/minikube/releases/download/v0.28.2/minikube-linux-amd64 -q -O minikube
 chmod +x minikube
 CHANGE_MINIKUBE_NONE_USER=true
