a1e712a50590b3891e3ae0ce71cef0a428ae96a5
Austin Seto <austin.seto@ibm.com>
2020-12-16 15:55:20 -0500

    Update for docker image test

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
-while getopts t:d:b:u: flag;
+while getopts t:d: flag;
 do
     case "${flag}" in
         t) DATE="${OPTARG}";;
         d) DRIVER="${OPTARG}";;
-        b) BUILD="${OPTARG}";;
-        u) DOCKER_USERNAME="${OPTARG}";;
     esac
 done
 
-echo "Testing daily build image"
+echo "Testing latest OpenLiberty Docker image"
 
 sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install></configuration>" system/pom.xml inventory/pom.xml
-cat system/pom.xml
-cat inventory/pom.xml
+cat system/pom.xml inventory/pom.xml
 
-sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM "$DOCKER_USERNAME"/olguides:"$BUILD";g" system/Dockerfile inventory/Dockerfile
-cat system/Dockerfile
-cat inventory/Dockerfile
+sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM openliberty/daily:latest;g" system/Dockerfile inventory/Dockerfile
+cat system/Dockerfile inventory/Dockerfile
 
 sudo ../scripts/startMinikube.sh
+docker pull "openliberty/daily:latest"
 sudo ../scripts/testApp.sh
 sudo ../scripts/stopMinikube.sh
