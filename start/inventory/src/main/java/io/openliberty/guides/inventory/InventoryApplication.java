7c1e59e238a7877fe18c4f31fd90613977286861
Gilbert Kwan <gkwan@ca.ibm.com>
2022-02-15 15:42:58 -0500

    Update InventoryApplication.java

--- start/inventory/src/main/java/io/openliberty/guides/inventory/InventoryApplication.java
+++ start/inventory/src/main/java/io/openliberty/guides/inventory/InventoryApplication.java
@@ -9,7 +9,7 @@
  * Contributors:
  *     IBM Corporation - Initial implementation
  *******************************************************************************/
- // end::copyright[]
+// end::copyright[]
 package io.openliberty.guides.inventory;
 
 // JAX-RS
