4be2751b77b652ca914ca7e0b1262ba7e102be3a
t27gupta <tushar.gupta@uwaterloo.ca>
2022-05-04 15:02:41 -0400

    script changed to match Dockerfile

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -13,7 +13,7 @@ echo "Testing latest OpenLiberty Docker image"
 sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" system/pom.xml inventory/pom.xml
 cat system/pom.xml inventory/pom.xml
 
-sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-java8-openj9-ubi;FROM openliberty/daily:latest;g" system/Dockerfile inventory/Dockerfile
+sed -i "s;FROM icr.io/appcafe/open-liberty:full-java11-openj9-ubi;FROM openliberty/daily:latest;g" system/Dockerfile inventory/Dockerfile
 cat system/Dockerfile inventory/Dockerfile
 
 docker pull "openliberty/daily:latest"
