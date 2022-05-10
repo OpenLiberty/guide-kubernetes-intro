d5e38f038989adafce5017ec776b7c21a78708f8
t27gupta <tushar.gupta@uwaterloo.ca>
2022-05-10 14:13:50 -0400

    Health checks in start directory

---
+++ start/inventory/src/main/java/io/openliberty/guides/inventory/health/InventoryLivenessCheck.java
@@ -0,0 +1,41 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2019, 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::copyright[]
+package io.openliberty.guides.inventory.health;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import io.openliberty.guides.inventory.InventoryResource;
+
+import java.lang.management.MemoryMXBean;
+import java.lang.management.ManagementFactory;
+
+import org.eclipse.microprofile.health.Liveness;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+
+// tag::Liveness[]
+@Liveness
+// end::Liveness[]
+@ApplicationScoped
+public class InventoryLivenessCheck implements HealthCheck {
+
+  @Override
+  public HealthCheckResponse call() {
+      MemoryMXBean memBean = ManagementFactory.getMemoryMXBean();
+      long memUsed = memBean.getHeapMemoryUsage().getUsed();
+      long memMax = memBean.getHeapMemoryUsage().getMax();
+
+      return HealthCheckResponse.named(InventoryResource.class.getSimpleName()
+                                      + " Liveness Check")
+                                .status(memUsed < memMax * 0.9).build();
+  }
+}
---
+++ start/inventory/src/main/java/io/openliberty/guides/inventory/health/InventoryStartupCheck.java
@@ -0,0 +1,45 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::copyright[]
+// tag::InventoryStartupCheck[]
+package io.openliberty.guides.inventory.health;
+
+import java.lang.management.ManagementFactory;
+import io.openliberty.guides.inventory.InventoryResource;
+
+import com.sun.management.OperatingSystemMXBean;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Startup;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+// tag::Startup[]
+@Startup
+// end::Startup[]
+@ApplicationScoped
+public class InventoryStartupCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        OperatingSystemMXBean bean = (com.sun.management.OperatingSystemMXBean)
+        ManagementFactory.getOperatingSystemMXBean();
+        double cpuUsed = bean.getSystemCpuLoad();
+        String cpuUsage = String.valueOf(cpuUsed);
+        return HealthCheckResponse.named(InventoryResource.class
+                                            .getSimpleName() + " Startup Check")
+                                            .status(cpuUsed < 0.95).build();
+    }
+}
+
+// end::InventoryStartupCheck[]
