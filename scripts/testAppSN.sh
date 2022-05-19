bdfc8fba8d0d1ee6315de59d4f4f3a9a122ae3dd
Gilbert Kwan <gkwan@ca.ibm.com>
2022-05-19 13:00:25 -0400

    Update testAppSN.sh

--- scripts/testAppSN.sh
+++ scripts/testAppSN.sh
@@ -8,15 +8,14 @@ docker pull icr.io/appcafe/open-liberty:full-java11-openj9-ubi
 docker build -t system:1.0-SNAPSHOT system/.
 docker build -t inventory:1.0-SNAPSHOT inventory/.
 
-NAMESPACE_NAME=$(bx cr namespace-list | grep sn-labs- | sed 's/ *$//g')
-echo "${NAMESPACE_NAME}"
-docker tag inventory:1.0-SNAPSHOT us.icr.io/"${NAMESPACE_NAME}"/inventory:1.0-SNAPSHOT
-docker tag system:1.0-SNAPSHOT us.icr.io/"${NAMESPACE_NAME}"/system:1.0-SNAPSHOT
-docker push us.icr.io/"${NAMESPACE_NAME}"/inventory:1.0-SNAPSHOT
-docker push us.icr.io/"${NAMESPACE_NAME}"/system:1.0-SNAPSHOT
+echo "${SN_ICR_NAMESPACE}"
+docker tag inventory:1.0-SNAPSHOT us.icr.io/"${SN_ICR_NAMESPACE}"/inventory:1.0-SNAPSHOT
+docker tag system:1.0-SNAPSHOT us.icr.io/"${SN_ICR_NAMESPACE}"/system:1.0-SNAPSHOT
+docker push us.icr.io/"${SN_ICR_NAMESPACE}"/inventory:1.0-SNAPSHOT
+docker push us.icr.io/"${SN_ICR_NAMESPACE}"/system:1.0-SNAPSHOT
 
-sed -i 's=system:1.0-SNAPSHOT=us.icr.io/'"${NAMESPACE_NAME}"'/system:1.0-SNAPSHOT=g' kubernetes.yaml
-sed -i 's=inventory:1.0-SNAPSHOT=us.icr.io/'"${NAMESPACE_NAME}"'/inventory:1.0-SNAPSHOT=g' kubernetes.yaml
+sed -i 's=system:1.0-SNAPSHOT=us.icr.io/'"${SN_ICR_NAMESPACE}"'/system:1.0-SNAPSHOT=g' kubernetes.yaml
+sed -i 's=inventory:1.0-SNAPSHOT=us.icr.io/'"${SN_ICR_NAMESPACE}"'/inventory:1.0-SNAPSHOT=g' kubernetes.yaml
 sed -i 's=nodePort: 31000==g' kubernetes.yaml
 sed -i 's=nodePort: 32000==g' kubernetes.yaml
 
@@ -28,9 +27,8 @@ kubectl get pods
 
 kubectl proxy &
 
-NAMESPACE_NAME=$(bx cr namespace-list | grep sn-labs- | sed 's/ //g')
-SYSTEM_PROXY=localhost:8001/api/v1/namespaces/"$NAMESPACE_NAME"/services/system-service/proxy
-INVENTORY_PROXY=localhost:8001/api/v1/namespaces/"$NAMESPACE_NAME"/services/inventory-service/proxy
+SYSTEM_PROXY=localhost:8001/api/v1/namespaces/"${SN_ICR_NAMESPACE}"/services/system-service/proxy
+INVENTORY_PROXY=localhost:8001/api/v1/namespaces/"${SN_ICR_NAMESPACE}"/services/inventory-service/proxy
 
 echo "$SYSTEM_PROXY" && echo "$INVENTORY_PROXY"
 
