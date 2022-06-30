249850cabce75fde7252b6b105cff8eb47de8c64
Gilbert Kwan <gkwan@ca.ibm.com>
2022-06-30 12:19:14 -0400

    Update startMinikube.sh

--- scripts/startMinikube.sh
+++ scripts/startMinikube.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Set up and start Minikube
-curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x kubectl
-ln -s -f "$(pwd)/kubectl" "/usr/local/bin/kubectl"
+#curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+#chmod +x kubectl
+#ln -s -f "$(pwd)/kubectl" "/usr/local/bin/kubectl"
 #wget https://github.com/kubernetes/minikube/releases/download/v0.28.2/minikube-linux-amd64 -q -O minikube
 #chmod +x minikube
 
