e778d269f69c7efac6ffdac9ba7ff25d0cd2d4c7
Gilbert Kwan <gkwan@ca.ibm.com>
2022-06-30 10:22:45 -0400

    Update startMinikube.sh

--- scripts/startMinikube.sh
+++ scripts/startMinikube.sh
@@ -3,14 +3,14 @@
 # Set up and start Minikube
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
-ln -s -f "$(pwd)/kubectl" "/usr/local/bin/kubectl"
+sudo ln -s -f "$(pwd)/kubectl" "/usr/local/bin/kubectl"
 #wget https://github.com/kubernetes/minikube/releases/download/v0.28.2/minikube-linux-amd64 -q -O minikube
 #chmod +x minikube
 
-apt-get update -y
-apt-get install -y conntrack
+sudo apt-get update -y
+sudo apt-get install -y conntrack
 
 sysctl fs.protected_regular=0
 
-minikube start --driver=none --bootstrapper=kubeadm
+sudo minikube start --driver=none --bootstrapper=kubeadm
 #eval "$(minikube docker-env)"
