4e4af5444dacaf14edb252ea1e66944e323a80a9
Gilbert Kwan <gkwan@ca.ibm.com>
2022-07-05 11:38:31 -0400

    Update startMinikube.sh

--- scripts/startMinikube.sh
+++ scripts/startMinikube.sh
@@ -26,6 +26,11 @@ wget "https://github.com/Mirantis/cri-dockerd/releases/download/v${VER}/cri-dock
 tar xvf "cri-dockerd-${VER}.amd64.tgz"
 mv cri-dockerd/cri-dockerd /usr/local/bin/
 cri-dockerd --version
+wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.service
+wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.socket
+mv cri-docker.socket cri-docker.service /etc/systemd/system/
+sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+
 systemctl daemon-reload
 systemctl enable cri-docker.service
 systemctl enable --now cri-docker.socket
