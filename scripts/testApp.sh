0de44e2045e7fbf6cce6939bd7d610b9b32a04d7
Gilbert Kwan <gkwan@ca.ibm.com>
2022-07-05 12:36:21 -0400

    Update testApp.sh

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -11,7 +11,8 @@ docker pull icr.io/appcafe/open-liberty:full-java11-openj9-ubi
 docker build -t system:1.0-SNAPSHOT system/.
 docker build -t inventory:1.0-SNAPSHOT inventory/.
 
-eval $(minikube docker-env -u)
+eval "$(minikube docker-env -u)"
+kubectl get services
 
 kubectl apply -f kubernetes.yaml
 
