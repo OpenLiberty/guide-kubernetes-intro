7cd18c7766ab933452873947f7bc7c5b453948d3
Gilbert Kwan <gkwan@ca.ibm.com>
2022-07-05 09:59:59 -0400

    Update startMinikube.sh

--- scripts/startMinikube.sh
+++ scripts/startMinikube.sh
@@ -21,8 +21,8 @@
 
 
 VER=$(curl -s https://api.github.com/repos/Mirantis/cri-dockerd/releases/latest|grep tag_name | cut -d '"' -f 4|sed 's/v//g')
-echo $VER
-wget https://github.com/Mirantis/cri-dockerd/releases/download/v${VER}/cri-dockerd-${VER}.amd64.tgz
-tar xvf cri-dockerd-${VER}.amd64.tgz
+echo "$VER"
+wget "https://github.com/Mirantis/cri-dockerd/releases/download/v${VER}/cri-dockerd-${VER}.amd64.tgz"
+tar xvf "cri-dockerd-${VER}.amd64.tgz"
 mv cri-dockerd/cri-dockerd /usr/local/bin/
 cri-dockerd --version
