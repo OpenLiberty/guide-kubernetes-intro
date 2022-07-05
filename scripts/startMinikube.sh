fbff8acd9b3a1373ed5954533ec04bc8d35f6190
Gilbert Kwan <gkwan@ca.ibm.com>
2022-07-05 09:50:52 -0400

    Update startMinikube.sh

--- scripts/startMinikube.sh
+++ scripts/startMinikube.sh
@@ -12,9 +12,17 @@
 
 #sysctl fs.protected_regular=0
 
-eval "$(minikube docker-env -u)"
-minikube stop
-minikube delete
+#eval "$(minikube docker-env -u)"
+#minikube stop
+#minikube delete
 
-minikube start --driver=none --bootstrapper=kubeadm
-eval "$(minikube docker-env)"
+#minikube start --driver=none --bootstrapper=kubeadm
+#eval "$(minikube docker-env)"
+
+
+VER=$(curl -s https://api.github.com/repos/Mirantis/cri-dockerd/releases/latest|grep tag_name | cut -d '"' -f 4|sed 's/v//g')
+echo $VER
+wget https://github.com/Mirantis/cri-dockerd/releases/download/v${VER}/cri-dockerd-${VER}.amd64.tgz
+tar xvf cri-dockerd-${VER}.amd64.tgz
+mv cri-dockerd/cri-dockerd /usr/local/bin/
+cri-dockerd --version
