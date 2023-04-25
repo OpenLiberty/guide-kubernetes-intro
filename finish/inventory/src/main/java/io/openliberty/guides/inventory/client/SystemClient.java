9c21da63fab328480d57c7ea81332a9e67957e77
Gilbert Kwan <gkwan@ca.ibm.com>
2023-04-25 12:08:21 -0400

    Update SystemClient.java

--- finish/inventory/src/main/java/io/openliberty/guides/inventory/client/SystemClient.java
+++ finish/inventory/src/main/java/io/openliberty/guides/inventory/client/SystemClient.java
@@ -29,51 +29,50 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 @RequestScoped
 public class SystemClient {
 
-  // Constants for building URI to the system service.
-  private final String SYSTEM_PROPERTIES = "/system/properties";
-  private final String PROTOCOL = "http";
+    // Constants for building URI to the system service.
+    private final String SYSTEM_PROPERTIES = "/system/properties";
+    private final String PROTOCOL = "http";
 
-  @Inject
-  @ConfigProperty(name = "default.http.port")
-  String DEFAULT_PORT;
+    @Inject
+    @ConfigProperty(name = "default.http.port")
+    String DEFAULT_PORT;
 
-  // Wrapper function that gets properties
-  public Properties getProperties(String hostname) {
-    Properties properties = null;
-    Client client = ClientBuilder.newClient();
-    try {
-        Builder builder = getBuilder(hostname, client);
-        properties = getPropertiesHelper(builder);
-    } catch (Exception e) {
-        System.err.println(
-        "Exception thrown while getting properties: " + e.getMessage());
-    } finally {
-        client.close();
+    // Wrapper function that gets properties
+    public Properties getProperties(String hostname) {
+        Properties properties = null;
+        Client client = ClientBuilder.newClient();
+        try {
+            Builder builder = getBuilder(hostname, client);
+            properties = getPropertiesHelper(builder);
+        } catch (Exception e) {
+            System.err.println(
+            "Exception thrown while getting properties: " + e.getMessage());
+        } finally {
+            client.close();
+        }
+        return properties;
     }
-    return properties;
-  }
 
-
-        // Method that creates the client builder
-        private Builder getBuilder(String hostname, Client client) throws Exception {
-          URI uri = new URI(
-                        PROTOCOL, null, hostname, Integer.valueOf(DEFAULT_PORT),
-                        SYSTEM_PROPERTIES, null, null);
-          String urlString = uri.toString();
-          Builder builder = client.target(urlString).request();
-          builder.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON);
-          return builder;
-      }
+    // Method that creates the client builder
+    private Builder getBuilder(String hostname, Client client) throws Exception {
+        URI uri = new URI(
+                      PROTOCOL, null, hostname, Integer.valueOf(DEFAULT_PORT),
+                      SYSTEM_PROPERTIES, null, null);
+        String urlString = uri.toString();
+        Builder builder = client.target(urlString).request();
+        builder.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON);
+        return builder;
+    }
 
     // Helper method that processes the request
     private Properties getPropertiesHelper(Builder builder) throws Exception {
-      Response response = builder.get();
-      if (response.getStatus() == Status.OK.getStatusCode()) {
-          return response.readEntity(Properties.class);
-      } else {
-          System.err.println("Response Status is not OK.");
-          return null;
-      }
-  }
+        Response response = builder.get();
+        if (response.getStatus() == Status.OK.getStatusCode()) {
+            return response.readEntity(Properties.class);
+        } else {
+            System.err.println("Response Status is not OK.");
+            return null;
+        }
+    }
 
 }
