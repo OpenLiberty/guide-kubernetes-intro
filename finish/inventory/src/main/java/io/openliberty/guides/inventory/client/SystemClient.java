b516ed943b8f1ece7481c8792552a6d4e98e828f
jakub-pomykala <56261244+jakub-pomykala@users.noreply.github.com>
2023-04-25 17:10:21 +0100

    Updating SystemClient.java (#238)
    * Updating SystemClient.java
    
    * updating copyright + start SystemClient
    
    * Update SystemClient.java
    
    * Update SystemClient.java
    
    ---------
    
    Co-authored-by: Gilbert Kwan <gkwan@ca.ibm.com>

--- finish/inventory/src/main/java/io/openliberty/guides/inventory/client/SystemClient.java
+++ finish/inventory/src/main/java/io/openliberty/guides/inventory/client/SystemClient.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2017, 2022 IBM Corporation and others.
+ * Copyright (c) 2017, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -29,75 +29,50 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 @RequestScoped
 public class SystemClient {
 
-  // Constants for building URI to the system service.
-  private final String SYSTEM_PROPERTIES = "/system/properties";
-  private final String PROTOCOL = "http";
+    // Constants for building URI to the system service.
+    private final String SYSTEM_PROPERTIES = "/system/properties";
+    private final String PROTOCOL = "http";
 
-  @Inject
-  @ConfigProperty(name = "default.http.port")
-  String DEFAULT_PORT;
+    @Inject
+    @ConfigProperty(name = "default.http.port")
+    String DEFAULT_PORT;
 
-  // Wrapper function that gets properties
-  public Properties getProperties(String hostname) {
-    String url = buildUrl(PROTOCOL, hostname, Integer.valueOf(DEFAULT_PORT),
-                          SYSTEM_PROPERTIES);
-    Builder clientBuilder = buildClientBuilder(url);
-    return getPropertiesHelper(clientBuilder);
-  }
-
-  // tag::doc[]
-  /**
-   * Builds the URI string to the system service for a particular host.
-   * @param protocol
-   *          - http or https.
-   * @param host
-   *          - name of host.
-   * @param port
-   *          - port number.
-   * @param path
-   *          - Note that the path needs to start with a slash!!!
-   * @return String representation of the URI to the system properties service.
-   */
-  // end::doc[]
-  protected String buildUrl(String protocol, String host, int port, String path) {
-    try {
-      URI uri = new URI(protocol, null, host, port, path, null, null);
-      return uri.toString();
-    } catch (Exception e) {
-      System.err.println("Exception thrown while building the URL: " + e.getMessage());
-      return null;
+    // Wrapper function that gets properties
+    public Properties getProperties(String hostname) {
+        Properties properties = null;
+        Client client = ClientBuilder.newClient();
+        try {
+            Builder builder = getBuilder(hostname, client);
+            properties = getPropertiesHelper(builder);
+        } catch (Exception e) {
+            System.err.println(
+            "Exception thrown while getting properties: " + e.getMessage());
+        } finally {
+            client.close();
+        }
+        return properties;
     }
-  }
 
-  // Method that creates the client builder
-  protected Builder buildClientBuilder(String urlString) {
-    try {
-      Client client = ClientBuilder.newClient();
-      Builder builder = client.target(urlString).request();
-      return builder.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON);
-    } catch (Exception e) {
-      System.err.println("Exception thrown while building the client: "
-                         + e.getMessage());
-      return null;
+    // Method that creates the client builder
+    private Builder getBuilder(String hostname, Client client) throws Exception {
+        URI uri = new URI(
+                      PROTOCOL, null, hostname, Integer.valueOf(DEFAULT_PORT),
+                      SYSTEM_PROPERTIES, null, null);
+        String urlString = uri.toString();
+        Builder builder = client.target(urlString).request();
+        builder.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON);
+        return builder;
     }
-  }
 
-  // Helper method that processes the request
-  protected Properties getPropertiesHelper(Builder builder) {
-    try {
-      Response response = builder.get();
-      if (response.getStatus() == Status.OK.getStatusCode()) {
-        return response.readEntity(Properties.class);
-      } else {
-        System.err.println("Response Status is not OK.");
-      }
-    } catch (RuntimeException e) {
-      System.err.println("Runtime exception: " + e.getMessage());
-    } catch (Exception e) {
-      System.err.println("Exception thrown while invoking the request: "
-                         + e.getMessage());
+    // Helper method that processes the request
+    private Properties getPropertiesHelper(Builder builder) throws Exception {
+        Response response = builder.get();
+        if (response.getStatus() == Status.OK.getStatusCode()) {
+            return response.readEntity(Properties.class);
+        } else {
+            System.err.println("Response Status is not OK.");
+            return null;
+        }
     }
-    return null;
-  }
 
 }
