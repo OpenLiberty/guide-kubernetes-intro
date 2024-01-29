022cc03772547e8e5841890ac896be94aa052aca
Gilbert Kwan <gkwan@ca.ibm.com>
2024-01-29 15:45:04 -0500

    update versions

--- start/inventory/src/main/java/io/openliberty/guides/inventory/client/SystemClient.java
+++ start/inventory/src/main/java/io/openliberty/guides/inventory/client/SystemClient.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2017, 2023 IBM Corporation and others.
+ * Copyright (c) 2017, 2024 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -34,8 +34,8 @@ public class SystemClient {
     private final String PROTOCOL = "http";
 
     @Inject
-    @ConfigProperty(name = "default.http.port")
-    String DEFAULT_PORT;
+    @ConfigProperty(name = "http.port")
+    String HTTP_PORT;
 
     // Wrapper function that gets properties
     public Properties getProperties(String hostname) {
@@ -56,7 +56,7 @@ public class SystemClient {
     // Method that creates the client builder
     private Builder getBuilder(String hostname, Client client) throws Exception {
         URI uri = new URI(
-                      PROTOCOL, null, hostname, Integer.valueOf(DEFAULT_PORT),
+                      PROTOCOL, null, hostname, Integer.valueOf(HTTP_PORT),
                       SYSTEM_PROPERTIES, null, null);
         String urlString = uri.toString();
         Builder builder = client.target(urlString).request();
